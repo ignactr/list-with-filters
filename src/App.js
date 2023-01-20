fea9bda3e0753e8fbc6ae561ceb82b44b6ed1534
Ignacy Trocki <ignacy.trocki@wp.pl>
2023-01-21 00:09:51 +0100

    added dark mode for form

--- src/App.js
+++ src/App.js
@@ -48,17 +48,23 @@ function Add(props){
   }
 
   return (
+    <div>
+      <Helmet>
+        <title>People List</title>
+        <html className={props.mode === true ? 'dark bg-gray-800' : ''} />
+      </Helmet>
+      <button onClick={()=> props.setMode(!props.mode)} className='hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 mt-2 ml-2 w-12 h-12 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700'><img src={ props.mode === true ? sun : moon} alt="sun icon" /></button>
     <div className="flex place-content-center">
       <div className="flex flex-col place-content-center">
         <form onSubmit={handleAdding}>
-          <div className={validation === '' ? 'py-9 px-8 text-xl flex flex-col space-x-2 mb-3 mt-16 border-solid border-2 rounded-lg border-slate-900 bg-slate-50' : 'pt-9 pb-4 px-8 text-xl flex flex-col space-x-2 mb-3 mt-16 border-solid border-2 rounded-lg border-slate-900 bg-slate-50'}>
+          <div className={validation === '' ? 'py-9 px-8 text-xl flex flex-col space-x-2 mb-3 mt-16 border-solid border-2 rounded-lg border-slate-900 bg-slate-50 dark:bg-zinc-500 dark:text-slate-100' : 'pt-9 pb-4 px-8 text-xl flex flex-col space-x-2 mb-3 mt-16 border-solid border-2 rounded-lg border-slate-900 bg-slate-50'}>
             <table className='text-center w-full'>
               <tbody>
                 <tr>
-                  <td>Imię</td><td><input value={name} onChange={event => setName(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100"/><br/></td>
+                  <td>Imię</td><td><input value={name} onChange={event => setName(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100 dark:bg-zinc-600 dark:border-zinc-600"/><br/></td>
                 </tr>
                 <tr>
-                  <td>Nazwisko</td><td><input value={surname} onChange={event => setSurname(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100"/></td>
+                  <td>Nazwisko</td><td><input value={surname} onChange={event => setSurname(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100 dark:bg-zinc-600 dark:border-zinc-600"/></td>
                 </tr>
               </tbody>
             </table>
@@ -67,7 +73,7 @@ function Add(props){
                 <div className='mt-2'>
                   <div className="flex justify-center">
                     <div className="form-check form-switch">
-                      <input onChange={toggle} className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-slate-900 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault56"/>
+                      <input onChange={toggle} className="form-check-input appearance-none w-9 -ml-10 rounded-full float-left h-5 align-top bg-no-repeat bg-contain bg-zinc-600 focus:outline-none cursor-pointer shadow-sm" type="checkbox" role="switch" id="flexSwitchCheckDefault56"/>
                     </div>
                   </div>
                 </div>
@@ -76,11 +82,12 @@ function Add(props){
             <p className='text-red-900 text-sm'>{validation}</p>
           </div>
           <div className='flex justify-center gap-2'>
-            <button onClick={()=>props.setPage(1)} className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 ">Wróć</button>
-            <input type="submit" className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 " value="Dodaj"/>
+            <button onClick={()=>props.setPage(1)} className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 dark:bg-gray-600 dark:hover:bg-gray-700 dark:disabled:bg-gray-700 dark:border-gray-600">Wróć</button>
+            <input type="submit" className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 dark:bg-gray-600 dark:hover:bg-gray-700 dark:disabled:bg-gray-700 dark:border-gray-600" value="Dodaj"/>
           </div>
         </form>
       </div>
+    </div>
     </div>
   )
 }
@@ -125,7 +132,7 @@ function App() {
   }
   else{
     return (
-      <Add add={add} setPage={setPage}/>
+      <Add add={add} setPage={setPage} mode={mode} setMode={()=>setMode(!mode)}/>
     )
   }
 }
