b7cfc9c64c2d97cfc6a01a348f8d702377db4913
Ignacy Trocki <ignacy.trocki@wp.pl>
2023-01-21 17:36:32 +0100

    added delete function and animations

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React, {useState} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import {Helmet} from "react-helmet";
 import sun from "../images/sun.png";
-import moon from "../images/moon.png"
+import moon from "../images/moon.png";
+import bin from "../images/bin.png";
 import Add from './Add';
 import List from './List';
 
 function App() {
   const [mode, setMode] = useState(false);
+  const [deleteMode, setDeleteMode] = useState(false);
   const [filter, setFilter] = useState('w');
   const [page, setPage] = useState(1);
   const [people, setPeople] = useState([
@@ -19,15 +21,17 @@ function App() {
     {id: uuidv4(), name: "Marcel", surname:"Afelierowietzei", sex:"m"}
   ]);
   const add = (person) => setPeople([...people, person]); 
+  const deleteFunc = (id) => setPeople(people.filter(person => person.id !== id));
 
   if(page === 1){
     return (
       <div>
         <Helmet>
           <title>People List</title>
-          <html className={mode === true ? 'dark bg-gray-800' : ''} />
+          <html className={mode === true ? 'dark bg-gray-800 m-0' : 'm-0'} />
         </Helmet>
-        <button onClick={()=> setMode(!mode)} className='hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 mt-2 ml-2 w-12 h-12 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700'><img src={ mode === true ? sun : moon} alt="sun icon" /></button>
+        <button onClick={()=> setMode(!mode)} className='mt-2 hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 ml-2 w-12 h-12 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700'><img src={ mode === true ? sun : moon} alt="sun icon" /></button>
+        <button onClick={()=> setDeleteMode(!deleteMode)} className={deleteMode === false ? 'marker:mt-2 hover:bg-slate-300 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-300 rounded-md p-1 ml-2 w-12 h-12 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 hover:shadow-lg hover:shadow-red-800/50' : 'delete mt-2 hover:bg-red-500 bg-red-600 border-solid border-2 border-red-600 hover:border-red-500 rounded-md p-1 ml-2 w-12 h-12 hover:shadow-lg hover:shadow-red-900/50'}><img src={bin} alt="sun icon" /></button>
         <div className="flex place-content-center">
           <div className="flex flex-col place-content-center"> 
             <div className="flex space-x-2 mb-3 mt-16">
@@ -35,9 +39,9 @@ function App() {
               <button onClick={() => setFilter('m')} disabled={filter === 'm' ? true : false} className="Button">Mężczyźni</button>
               <button onClick={() => setFilter('k')} disabled={filter === 'k' ? true : false} className="Button">Kobiety</button>
             </div>
-            <List filter = {filter} people={people}/>
+            <List filter = {filter} people={people} deleteMode={deleteMode} deleteFunc={deleteFunc}/>
             <div className=' flex place-content-center'>
-              <button onClick={()=>setPage(2)} className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-2xl hover:bg-slate-700 hover:border-slate-800 w-12 text-center py-1 dark:bg-gray-600 dark:hover:bg-gray-700">+</button>
+              <button onClick={deleteMode === false ? ()=>setPage(2) : ()=> setDeleteMode(!deleteMode)} className={deleteMode === true ?'Bottom-Button-1 delete' : 'Bottom-Button-2'}>{deleteMode === true ? 'x' : '+'}</button>
             </div>
           </div>
         </div>
--- src/components/List.js
+++ src/components/List.js
@@ -6,8 +6,8 @@ function List(props) {
     return (
       <div className="text-center">
         { props.filter === "w" ?
-          people.map(person => <p key={person.id} className='Person'>{person.name} {person.surname}</p>) :
-          filtered.map(person => <p key={person.id} className='Person'>{person.name} {person.surname}</p>)
+          people.map(person => <><button onClick={()=>props.deleteFunc(person.id)} disabled={props.deleteMode === true ? false : true} key={person.id} className='Person'>{person.name} {person.surname}</button><br/></>) :
+          filtered.map(person => <><button onClick={()=>props.deleteFunc(person.id)} disabled={props.deleteMode === true ? false : true} key={person.id} className='Person'>{person.name} {person.surname}</button><br/></>)
         }
       </div>
     );
