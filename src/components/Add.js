1b78c99d157a5dff83c459e3d64310e3a549a827
Ignacy Trocki <ignacy.trocki@wp.pl>
2023-01-22 14:02:05 +0100

    style changes + english language option

--- src/components/Add.js
+++ src/components/Add.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import {Helmet} from "react-helmet";
 import sun from "../images/sun.png";
 import moon from "../images/moon.png"
+import globe from "../images/globe.png";
 //function Add shows a form with three inputs and uses add function provided by parent component (App.js) 
 function Add(props){
     const [name, setName] = useState('');
@@ -39,6 +40,7 @@ function Add(props){
           <html className={props.mode === true ? 'dark bg-gray-800' : ''} />
         </Helmet>
         <button onClick={()=> props.setMode(!props.mode)} className='hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 mt-2 ml-2 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700'><img className='w-11 h-11' src={ props.mode === true ? sun : moon} alt="sun icon" /></button>
+        <button onClick={props.changeLanguage} className='mt-2 hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 ml-2 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 hover:shadow-lg hover:shadow-slate-900/50'><img className='w-11 h-11' src={globe} alt="sun icon" /></button>
       <div className="flex place-content-center">
         <div className="flex flex-col place-content-center">
           <form onSubmit={handleAdding}>
@@ -46,15 +48,15 @@ function Add(props){
               <table className='text-center w-full'>
                 <tbody>
                   <tr>
-                    <td>Imię</td><td><input value={name} onChange={event => setName(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100 dark:bg-zinc-600 dark:border-zinc-600"/><br/></td>
+                    <td>{props.language === 'pl' ? 'Imię': 'Name'}</td><td><input value={name} onChange={event => setName(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100 dark:bg-zinc-600 dark:border-zinc-600"/><br/></td>
                   </tr>
                   <tr>
-                    <td>Nazwisko</td><td><input value={surname} onChange={event => setSurname(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100 dark:bg-zinc-600 dark:border-zinc-600"/></td>
+                    <td>{props.language === 'pl' ? 'Nazwisko': 'Surname'}</td><td><input value={surname} onChange={event => setSurname(event.target.value)} type="text" className="border-solid border-2 rounded-lg border-slate-200 focus:border-slate-400 focus:bg-slate-100 dark:bg-zinc-600 dark:border-zinc-600"/></td>
                   </tr>
                 </tbody>
               </table>
               <div className='w-full flex justify-center'>
-                <div className='text-md mt-1.5'>Kobieta</div>
+                <div className='text-md mt-1.5'>{props.language === 'pl' ? 'Kobieta': 'Female'}</div>
                   <div className='mt-2'>
                     <div className="flex justify-center">
                       <div className="form-check form-switch">
@@ -62,13 +64,13 @@ function Add(props){
                       </div>
                     </div>
                   </div>
-                <div className='text-md mt-1.5'>Mężczyzna</div>
+                <div className='text-md mt-1.5 min-w-[30%]'>{props.language === 'pl' ? 'Mężczyzna': 'Male'}</div>
               </div>
               <p className='text-red-900 text-sm'>{validation}</p>
             </div>
             <div className='flex justify-center gap-2'>
-              <button onClick={()=>props.setPage(1)} className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 dark:bg-gray-600 dark:hover:bg-gray-700 dark:disabled:bg-gray-700 dark:border-gray-600">Wróć</button>
-              <input type="submit" className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 dark:bg-gray-600 dark:hover:bg-gray-700 dark:disabled:bg-gray-700 dark:border-gray-600" value="Dodaj"/>
+              <button onClick={()=>props.setPage(1)} className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 dark:bg-gray-600 dark:hover:bg-gray-700 dark:disabled:bg-gray-700 dark:border-gray-600">{props.language === 'pl' ? 'Wróć': 'Return'}</button>
+              <input type="submit" className="border-solid border-2 mt-2 bg-slate-900 text-slate-50 border-slate-900 rounded-lg text-xl hover:bg-slate-700 hover:border-slate-800 w-16 text-center py-1 dark:bg-gray-600 dark:hover:bg-gray-700 dark:disabled:bg-gray-700 dark:border-gray-600" value={props.language === 'pl' ? 'Dodaj': 'Add'}/>
             </div>
           </form>
         </div>
--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import {Helmet} from "react-helmet";
 import sun from "../images/sun.png";
 import moon from "../images/moon.png";
 import bin from "../images/bin.png";
+import globe from "../images/globe.png";
 import Add from './Add';
 import List from './List';
 
@@ -13,14 +14,21 @@ function App() {
   const [deleteMode, setDeleteMode] = useState(false);
   const [filter, setFilter] = useState('w');
   const [page, setPage] = useState(1);
+  const [language, setLanguage] = useState('eng');
   const [people, setPeople] = useState([
     {id: uuidv4(), name: "Bubi", surname:"Bałysz", sex:"m"},
     {id: uuidv4(), name: "Maria", surname:"Gitara", sex:"k"},
     {id: uuidv4(), name: "Kondron", surname:"Kiewitz", sex:"m"},
     {id: uuidv4(), name: "Hania", surname:"Montania", sex:"k"},
     {id: uuidv4(), name: "Bania", surname:"Cyganna", sex:"k"},
-    {id: uuidv4(), name: "Marcel", surname:"Afelierowietzei", sex:"m"}
+    {id: uuidv4(), name: "Marcel", surname:"Żółwik", sex:"m"}
   ]);
+  const changeLanguage = () =>{
+    if(language === 'pl') 
+      setLanguage('eng'); 
+    else 
+      setLanguage('pl');
+  }
   const add = (person) => setPeople([...people, person]); 
   const deleteFunc = (id) => setPeople(people.filter(person => person.id !== id));
 //page 1 is main list/page, page 2 is adding form
@@ -31,14 +39,15 @@ function App() {
           <title>People List</title>
           <html className={mode === true ? 'dark bg-gray-800 m-0' : 'm-0'} />
         </Helmet>
-        <button onClick={()=> setMode(!mode)} className='mt-2 hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 ml-2 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700'><img className='w-11 h-11' src={ mode === true ? sun : moon} alt="sun icon" /></button>
+        <button onClick={()=> setMode(!mode)} className='mt-2 hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 ml-2 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 hover:shadow-lg hover:shadow-slate-900/50'><img className='w-11 h-11' src={ mode === true ? sun : moon} alt="sun icon" /></button>
         <button onClick={()=> setDeleteMode(!deleteMode)} className={deleteMode === false ? 'mt-2 hover:bg-slate-300 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-300 rounded-md p-1 ml-2 w-14 h-14 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 hover:shadow-lg hover:shadow-red-800/50' : 'delete mt-2 hover:bg-red-500 bg-red-600 border-solid border-2 border-red-600 hover:border-red-500 rounded-md p-1 ml-2 w-14 h-14 hover:shadow-lg hover:shadow-red-900/50'}><img src={bin} alt="sun icon" /></button>
+        <button onClick={changeLanguage} className='mt-2 hover:bg-slate-200 bg-slate-100 border-solid border-2 border-slate-100 hover:border-slate-200 rounded-md p-1 ml-2 dark:bg-gray-600 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 hover:shadow-lg hover:shadow-slate-900/50'><img className='w-11 h-11' src={globe} alt="sun icon" /></button>
         <div className="flex place-content-center">
           <div className="flex flex-col place-content-center"> 
             <div className="flex space-x-2 mb-3 mt-16">
-              <button onClick={() => setFilter('w')} disabled={filter === 'w' ? true : false} className="Button">Wszystko</button>
-              <button onClick={() => setFilter('m')} disabled={filter === 'm' ? true : false} className="Button">Mężczyźni</button>
-              <button onClick={() => setFilter('k')} disabled={filter === 'k' ? true : false} className="Button">Kobiety</button>
+              <button onClick={() => setFilter('w')} disabled={filter === 'w' ? true : false} className="Button">{language === 'pl' ? 'Wszystko': 'Everyone'}</button>
+              <button onClick={() => setFilter('m')} disabled={filter === 'm' ? true : false} className="Button">{language === 'pl' ? 'Mężczyźni': 'Males'}</button>
+              <button onClick={() => setFilter('k')} disabled={filter === 'k' ? true : false} className="Button">{language === 'pl' ? 'Kobiety': 'Females'}</button>
             </div>
             <List filter = {filter} people={people} deleteMode={deleteMode} deleteFunc={deleteFunc}/>
             <div className='flex place-content-center'>
@@ -52,7 +61,7 @@ function App() {
   else{
     return (
       //returning form for adding new data (People)
-      <Add add={add} setPage={setPage} mode={mode} setMode={()=>setMode(!mode)}/>
+      <Add add={add} setPage={setPage} mode={mode} setMode={()=>setMode(!mode)} language={language} changeLanguage={changeLanguage}/>
     )
   }
 }
